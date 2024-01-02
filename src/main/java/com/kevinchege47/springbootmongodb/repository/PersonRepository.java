5e01ba7ff6b1182fd6aee4253421cdfe0c8f13bb
kevinchege47 <chegekevin001@gmail.com>
2024-01-02 12:03:17 +0300

    added mongo template

--- src/main/java/com/kevinchege47/springbootmongodb/repository/PersonRepository.java
+++ src/main/java/com/kevinchege47/springbootmongodb/repository/PersonRepository.java
@@ -2,8 +2,23 @@ package com.kevinchege47.springbootmongodb.repository;
 
 import com.kevinchege47.springbootmongodb.collection.Person;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
-public interface PersonRepository extends MongoRepository<Person,String> {
+public interface PersonRepository extends MongoRepository<Person, String> {
+    //    This is a case sensitive search.
+//    List<Person> findByFirstNameStartsWith(String name);
+
+    //    This is a case insensitive search.
+
+    List<Person> findByFirstNameStartsWithIgnoreCase(String name);
+
+//    List<Person> findByAgeBetween(Integer minAge, Integer maxAge);
+    @Query(value = "{'age':{$gt:?0,$lte:?1}}",fields = "{addresses: 0}")
+    List<Person> findPersonByAgeBetween(Integer minAge, Integer maxAge);
+
+
 }
--- src/main/java/com/kevinchege47/springbootmongodb/service/PersonService.java
+++ src/main/java/com/kevinchege47/springbootmongodb/service/PersonService.java
@@ -1,7 +1,19 @@
 package com.kevinchege47.springbootmongodb.service;
 
 import com.kevinchege47.springbootmongodb.collection.Person;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import java.util.List;
 
 public interface PersonService {
     String save(Person person);
+
+    List<Person> getPersonStartWith(String name);
+
+    void delete(String id);
+
+    List<Person> getByPersonAge(Integer minAge, Integer maxAge);
+
+    Page<Person> search(String name, Integer minAge, Integer maxAge, String city, Pageable pageable);
 }
